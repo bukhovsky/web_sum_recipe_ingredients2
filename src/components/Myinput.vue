9f162e5d67616d918198caf9baaa26f50b37ebb0
unknown <bukhovsky@gmail.com>
2022-03-29 09:42:59 +0300

    initial logic add

---
+++ src/components/Myinput.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+import { recipeanalyzer } from './recipeanalyzer.js'
+
+
+const ingredients = ref('')
+const recipe = ref('')
+
+function submit() {
+   ingredients.value = recipeanalyzer(recipe.value)
+}
+
+</script>
+
+<template>
+<h1>{{ ingredients }}</h1>
+
+<p style="white-space: pre-line;">{{ recipe }}</p>
+
+<textarea v-model="recipe" @keyup="submit(message)" placeholder="add multiple lines" rows="10" cols="30"></textarea>
+
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
