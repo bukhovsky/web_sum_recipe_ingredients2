e3ae8eb172b2cf146eb1afb34ef3d4486bccab03
unknown <bukhovsky@gmail.com>
2022-04-02 14:55:00 +0300

    css some  add

--- src/App.vue
+++ src/App.vue
@@ -13,10 +13,6 @@ import Myinput from './components/Myinput.vue'
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: left;
-  color: #2c3e50;
-  margin-top: 50px;
+ 
 }
 </style>
--- src/components/Myinput.vue
+++ src/components/Myinput.vue
@@ -16,35 +16,86 @@ function submit() {
 
 <h1>Рецептный граммовычислитель</h1>
 
+<div class = "mainarea">
+  
 
 <textarea v-model="recipe" @keyup="submit(message)" placeholder="добавьте рецепты" rows="10" cols="30"></textarea>
+
 <ul>
 <li v-for="item in ingredients">
   {{ item.ingridient }}
 </li>
 </ul>
+</div>
+
+<footer>
+  My footer
+</footer>
 
 </template>
 
 <style scoped>
-textarea {
-  float: left;
-  width: 60%;
+.mainarea {
+  display: flex;
+  flex-wrap: wrap;
+  border: 1px solid black;
   height: 500px;
-  padding: 15px;
-  font-size: 16px;
+  width: 100%;
   
 
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-
-  color: #2c3e50;
 }
+
+
+
+textarea {
+  flex: 2;
+  background-color: #dddddd;
+  color: #666666;
+  padding: 1em;
+  border-radius: 10px;
+  border: 2px solid transparent;
+  outline: none;
+  font-family: "Heebo", sans-serif;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 1.4;
+  
+  height: 500px;
+  transition: all 0.2s;
+}
+
+textarea:hover {
+  cursor: pointer;
+  background-color: #eeeeee;
+}
+
+textarea:focus {
+  cursor: text;
+  color: #333333;
+  background-color: white;
+  border-color: #333333;
+}
+
 
 ul {
-  padding: 30px;
-  float: left;
+  flex: 1;
+ margin: 0;
+ border: 1px solid black;
+  height: 500px;
+  /* width: 35%; */
   font-size: 16px;
 }
+
+
+  footer {
+    margin-top: 50px;
+  background-color: #777;
+  padding: 10px;
+  text-align: center;
+  color: white;
+  
+}
+
+
+
 </style>
